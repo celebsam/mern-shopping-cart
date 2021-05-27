17f8eec48c48c6ea45c6aa882308d0f050edd7d8
celebsam <samuelogbe0@gmail.com>
2021-05-27 17:35:23 +0100

    single product page completed

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
+import products from "../products";
+
+const ProductScreen = (props) => {
+   const { id } = props.match.params;
+   const product = products.find((p) => p._id === id);
+   return (
+      <>
+         <Link to="/" className="btn btn-secondary">
+            <i className="fas fa-angle-double-left"></i>
+         </Link>
+         <Row className="mt-4">
+            <Col md={5}>
+               <Image src={product.image} alt={product.name} fluid />
+            </Col>
+            <Col md={4}>
+               <ListGroup variant="flush">
+                  <ListGroup.Item>
+                     <h5>{product.name}</h5>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                     <Rating
+                        value={product.rating}
+                        text={`${product.numReviews}  reviews`}
+                     />
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                     <h5>&#8358;{product.price}</h5>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                     <p>{product.description}</p>
+                  </ListGroup.Item>
+               </ListGroup>
+            </Col>
+            <Col md={3}>
+               <Card>
+                  <ListGroup variant="flush">
+                     <ListGroup.Item>
+                        <Row>
+                           <Col>Price:</Col>
+                           <Col>&#8358;{product.price}</Col>
+                        </Row>
+                     </ListGroup.Item>
+                     <ListGroup.Item>
+                        <Row>
+                           <Col>Status:</Col>
+                           <Col>
+                              {product.countInStock > 0
+                                 ? "In stock"
+                                 : "Out of stock"}
+                           </Col>
+                        </Row>
+                     </ListGroup.Item>
+                     <ListGroup.Item>
+                        <Button
+                           style={{ width: "100%" }}
+                           className="btn-md btn-block btn-secondary"
+                           type="button"
+                           disabled={product.countInStock > 0 ? false : true}
+                        >
+                           Add To Cart
+                        </Button>
+                     </ListGroup.Item>
+                  </ListGroup>
+               </Card>
+            </Col>
+         </Row>
+      </>
+   );
+};
+
+export default ProductScreen;
