17d46d99bbaa872edb55326f1d7d2dddf0b197ec
celebsam <samuelogbe0@gmail.com>
2021-07-30 10:33:05 +0100

    Created order route and controller in the backend

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,33 @@
+import Order from "../models/orderModel.js";
+
+export const addOrderItems = (req, res) => {
+   const {
+      orderItems,
+      shippingPrice,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingAddress,
+      totalPrice,
+   } = req.body;
+
+   if (orderItems && orderItems.length === 0) {
+      res.status(400);
+      throw new Error("No odrder items");
+      return;
+   } else {
+      const order = new Order({
+         orderItems,
+         shippingPrice,
+         user: req.user._id,
+         paymentMethod,
+         itemsPrice,
+         taxPrice,
+         shippingAddress,
+         totalPrice,
+      });
+
+      const createdOrder = await order.save();
+      res.status(201).json(createdOrder);
+   }
+};
