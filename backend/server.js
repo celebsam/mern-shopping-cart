3f11060eae87aac6e3ac8aae8abbbd5d3071ce74
Samuel Green <samuelogbe0@gmail.com>
2022-12-30 10:08:15 +0100

    Updated the deployment code

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,13 @@ app.use("/api/orders", orderRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "./frontend/build")));
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running...");
+    res.send("API is running....");
   });
 }
 
